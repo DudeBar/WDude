61f2ca679952e02d4df6e0b31547bee32c6fca43
sebastienpro <spro@webgeoservices.com>
2014-06-11 10:36:55 +0200

    add first Test

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from website.models import Customer
+
+
+class CustomerTestCase(TestCase):
+
+    def test_customer_create(self):
+        response = self.client.post("/customer_create",
+                                    {
+                                    'login': "test",
+                                    'password': "test",
+                                    'confirm_password': "test"
+                                    })
+        self.assertEquals(response.status_code, 302)
+        self.assertTrue(Customer.objects.filter(login="test").exists())
+
