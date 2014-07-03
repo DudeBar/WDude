193140751232778e04f61c92732a557254e9d868
sebastienpro <spro@webgeoservices.com>
2014-07-03 13:36:42 +0200

    add auto-refresh in customers account page

--- website/urls.py
+++ website/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns(
     url(r'^login$', views.login, name='login'),
     url(r'^customer_create$', views.customer_create, name='customer_create'),
     url(r'^customer_account$', views.customer_account, name='customer_account'),
+    url(r'^customer_ajax_info/$', views.customer_ajax_info, name='customer_ajax_info'),
 
     url(r'^add_command$', views.add_command, name='add_command'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'bar_login.html'},
@@ -18,6 +19,7 @@ urlpatterns = patterns(
     url(r'^add_fidelity/(?P<customer_id>\d+)/$', views.add_fidelity, name='add_fidelity'),
     url(r'^add_fidelity/$', views.new_fidelity, name='new_fidelity'),
     url(r'^bade_fidelity$', views.bade_fidelity, name='bade_fidelity'),
+    url(r'^wheel$', views.wheel, name='wheel'),
 
     url(r'^barman_account$', views.barman_account, name='barman_account'),
     url(r'^customer_detail/(?P<customer_id>\d+)/$', views.customer_detail, name='customer_detail'),
